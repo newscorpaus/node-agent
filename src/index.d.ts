d4c620fcff47b95ce3c9d5b8f34d413a96e1d9dd
Faiz <faizn1@nca1031460.news.newslimited.local>
2017-10-11 09:46:36 +1100

    Correct factory name: _agent

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,4 +7,4 @@ export as namespace agent;
 
 export function Agent(protocol: string, url: string, headers: any, writable: NodeJS.WritableStream, cb: ((error?: Error, result?: any) => void)): void;
 
-export function _get(agent: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): void; 
+export function _agent(agent: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): void; 
