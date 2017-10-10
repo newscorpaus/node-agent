7955b2c78ffccd7578c8ec3f3f3d2c4070c90990
Faiz <faizn1@nca1031460.news.newslimited.local>
2017-10-11 10:59:18 +1100

    Use an interface to represent the function type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,9 @@
-/// reference types='request'
 /// <reference types='node' />
 
 import * as request from 'request';
 
-export as namespace agent;
+export interface Agent {
+    (protocol: string, url: string, headers: any, writable: NodeJS.WritableStream, cb: ((error?: Error, result?: any) => void)): void;
+}
 
-export function Agent(protocol: string, url: string, headers: any, writable: NodeJS.WritableStream, cb: ((error?: Error, result?: any) => void)): void;
-
-export function _agent(agent?: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): void; 
+export function _agent(agent?: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): Agent;
