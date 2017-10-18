fac8aa7e9e9049ddb195ea608061192750e0619a
James Moriarty <james.moriarty@news.com.au>
2017-10-19 10:31:21 +1100

    refinement: agent to accept url.

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,19 @@
 
 import { Writable } from 'stream';
 import { omit } from 'ramda';
+import { Url } from 'url';
 import * as _request from 'request';
+import { UrlOptions } from 'request';
 
 const _agent = (requester: _request.RequestAPI<_request.Request, _request.CoreOptions, _request.RequiredUriUrl> = _request) => {
-    return function (protocol: string, url: string, headers: any, writable: Writable, cb: ((error?: Error, result?: any) => void)): void {
+    return function (url: string, headers: any, writable: Writable, cb: ((error?: Error, result?: any) => void)): void {
         const headersCopy = omit(['host'], headers);
-
-        // remove the leading '/'
-        const uri = `${ protocol }://${ url.substr(1) }`;
-
-        // Express request.protocol looks like 'http:'. The request lib requires 'http' (no colon).
-        const requestProtocol = `${ protocol }:`;
 
         const options = {
             method: 'GET',
             headers: headersCopy,
-            uri: uri,
-            gzip: true,
-            protocol: requestProtocol
+            url: url,
+            gzip: true
         };
 
         requester(options, (err: Error, res: any) => {
