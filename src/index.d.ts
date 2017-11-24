691c3ba0a6f6201e6fae22fc3582f23f7fae6543
James Moriarty <james.moriarty@news.com.au>
2017-11-24 15:35:25 +1100

    feature: simple agent.
    * don't accept writeable / respond to client.
    * encapsulate request library with our types.
    * remove header (business) logic.

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types='node' />
-
-import * as request from 'request';
-
-export interface Agent {
-    (url: string, headers: any, writable: NodeJS.WritableStream, cb: ((error?: Error, result?: any) => void)): void;
-}
-
-export function _agent(agent?: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): Agent;
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
-/**
- * A streaming agent that takes a Writable stream from the outside (presumably for a HTTPResponse)
- * and streams an upstream response into it.
- */
+import * as defaultRequest from 'request';
 
-import { Writable } from 'stream';
-import { omit } from 'ramda';
-import { Url } from 'url';
-import * as _request from 'request';
-import { UrlOptions } from 'request';
-
-const _agent = (requester: _request.RequestAPI<_request.Request, _request.CoreOptions, _request.RequiredUriUrl> = _request) => {
-    return function (url: string, headers: any, writable: Writable, cb: ((error?: Error, result?: any) => void)): void {
-        const headersCopy = omit(['host'], headers);
-
+const agent = (request: defaultRequest.RequestAPI<defaultRequest.Request, defaultRequest.CoreOptions, defaultRequest.RequiredUriUrl> = defaultRequest): any => {
+    return function (url: string, headers: any, cb: (error: Error, result: any) => void): void {
         const options = {
             method: 'GET',
-            headers: headersCopy,
+            headers: headers,
             url: url,
             gzip: true
         };
 
-        requester(options, (err: Error, res: any) => {
+        request(options, (err: Error, res: any) => {
             if (err) {
-                cb(err, res);
+                cb(err, undefined);
             } else {
-                cb(undefined, res);
+                cb(undefined, {
+                    url: url,
+                    status: res.statusCode,
+                    headers: res.headers,
+                    body: res.body
+                });
             }
-        }).pipe(writable);
+        });
     };
 };
 
-export { _agent };
+export { agent };
